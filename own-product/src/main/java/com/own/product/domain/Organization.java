991d0cb56647426597ef26a5ea4ae3ed48a383ec
zhangy@proudkids.cn <Blucezhang@outlook.com>
2018-04-28 13:07:01 +0800

    get set ->@Data

--- own-product/src/main/java/com/own/product/domain/Organization.java
+++ own-product/src/main/java/com/own/product/domain/Organization.java
@@ -1,32 +1,18 @@
 package com.own.product.domain;
 
+import lombok.Data;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
 
+@Data
 @NodeEntity
 public class Organization extends Party{
 
 	@Property(name="name")
 	private String name;
-	
 	@Property(name="level")
 	private Integer level;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getLevel() {
-		return level;
-	}
-
-	public void setLevel(Integer level) {
-		this.level = level;
-	}
 
 }
  
--- own-product/src/main/java/com/own/product/domain/Template.java
+++ own-product/src/main/java/com/own/product/domain/Template.java
@@ -1,43 +1,18 @@
 package com.own.product.domain;
 
+import lombok.Data;
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
 
+@Data
 @NodeEntity
 public class Template {
-
 	@GraphId
 	private Long id;
-	
 	@Property(name="name")
 	private String name;
-	
 	@Property(name="content")
 	private String content;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
 }
