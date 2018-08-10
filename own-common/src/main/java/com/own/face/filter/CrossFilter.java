d656fef8f8e01c62211d89f3d01968f245a4294a
zhangy@proudkids.cn <Blucezhang@outlook.com>
2018-08-10 11:05:58 +0800

    update CrossFilter

--- own-common/src/main/java/com/own/face/filter/CrossFilter.java
+++ own-common/src/main/java/com/own/face/filter/CrossFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Slf4j
 @Component
